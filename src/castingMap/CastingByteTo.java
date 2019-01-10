b8d94367354011a0470f1b73c8f135f095e28dd4
= <ahmed.nabil.elagder@gmail.com>
2019-01-10 06:59:27 +0300

    add initially CastingByteTo

---
+++ src/castingMap/CastingByteTo.java
@@ -0,0 +1,23 @@
+package castingMap;
+
+public class CastingByteTo {
+
+	public static void main(String[] args) {
+
+		byte byteVar = 1; // any byte value
+		System.out.println("byteVar = " + byteVar);
+		
+		// casting byte to short
+		// BY Default - No casting required
+		short shortVar = byteVar;
+		System.out.println("shortVar = " + shortVar);
+		
+		
+		// casting byte to int
+		// BY Default - No casting required
+		int intVar = byteVar;
+		System.out.println("intVar = " + intVar);
+
+		
+	}
+}
