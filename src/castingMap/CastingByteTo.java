27cdb6f2acff1cc055aa4be0e2d205c866b94827
= <ahmed.nabil.elagder@gmail.com>
2019-01-10 15:36:02 +0300

    update Casting byte To

--- src/castingMap/CastingByteTo.java
+++ src/castingMap/CastingByteTo.java
@@ -1,23 +1,60 @@
 package castingMap;
 
+/**
+ * This class demonstrate how to cast byte primitive data type to others ones
+ * 
+ * @author Ahmed Nabil
+ *
+ */
 public class CastingByteTo {
+	
+	private static final String FROM = "byte";
 
 	public static void main(String[] args) {
 
-		byte byteVar = 1; // any byte value
+		byte byteVar = 65; // any byte value
+		System.out.println("===========================");
 		System.out.println("byteVar = " + byteVar);
-		
+		System.out.println("===========================");
+
 		// casting byte to short
 		// BY Default - No casting required
 		short shortVar = byteVar;
-		System.out.println("shortVar = " + shortVar);
-		
-		
+		System.out.println(Helper.getCastingMessage(FROM, "short") + "\t" + "shortVar = " + shortVar);
+
 		// casting byte to int
 		// BY Default - No casting required
 		int intVar = byteVar;
-		System.out.println("intVar = " + intVar);
+		System.out.println(Helper.getCastingMessage(FROM, "int") + "\t" + "intVar = " + intVar);
 
-		
+		// casting byte to long
+		// BY Default - No casting required
+		long longVar = byteVar;
+		System.out.println(Helper.getCastingMessage(FROM, "long") + "\t" + "longVar = " + longVar);
+
+		// casting byte to float
+		// BY Default - No casting required
+		float floatVar = byteVar;
+		System.out.println(Helper.getCastingMessage(FROM, "float") + "\t" + "floatVar = " + floatVar);
+
+		// casting byte to double
+		// BY Default - No casting required
+		double doubleVar = byteVar;
+		System.out.println(Helper.getCastingMessage(FROM, "double") + "\t" + "doubleVar = " + doubleVar);
+
+		// casting byte to boolean
+		// boolean booleanVar = true;
+		String hint = "[ InValid - Can NOT be casted ]";
+		System.out.println(Helper.getCastingMessage(FROM, "boolean") + "\t" + hint);
+
+		// casting byte to char
+		char charVar = (char) (byteVar);
+		hint = "[ It will print corresponding (ASCII) char ]";
+		System.out.println(Helper.getCastingMessage(FROM, "char") + "\t" + "charVar = " + charVar + "\t" + hint);
+
+		// casting byte to string
+		String stringVar = Byte.toString(byteVar);
+		System.out.println(Helper.getCastingMessage(FROM, "string") + "\t" + "stringVar = " + stringVar);
+
 	}
 }
---
+++ src/castingMap/Helper.java
@@ -0,0 +1,9 @@
+package castingMap;
+
+public class Helper {
+
+	public static String getCastingMessage(String from, String to) {
+		return "casting "+ from +" to " + to;
+	}
+
+}
