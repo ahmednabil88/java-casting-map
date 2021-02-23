44a1c4a107841d30d2afbc50c1fd07b502cbf682
ahmednabil88 <ahmed.nabil.elagder@gmail.com>
2021-02-23 22:24:38 +0100

    add sample test [for demo]

---
+++ src/test/java/java_casting_map/BooleanCastingTest.java
@@ -0,0 +1,16 @@
+package java_casting_map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BooleanCastingTest {
+
+    @Test
+    public void testMM() {
+        assertTrue(false);
+    }
+}
